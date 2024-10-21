f9cfa5dcdec2eb0a86593147392374c0b803dfd7
Abdelrahman Hussein <elhussieny100@gmail.com>
2024-10-21 19:04:10 +0300

    - added chat view - added chat bubble - added chat text field

--- lib/features/home/presentation/views/widgets/chat_list.dart
+++ lib/features/home/presentation/views/widgets/chat_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../../../../core/routes/routes.dart';
 import 'chat_list_item.dart';
 
 class ChatList extends StatelessWidget {
@@ -10,7 +12,9 @@ class ChatList extends StatelessWidget {
       child: ListView.builder(
         itemCount: 10,
         itemBuilder: (context, index) {
-          return const ChatListItem();
+          return ChatListItem(
+            onTap: () => context.push(Routes.chatView),
+          );
         },
       ),
     );
